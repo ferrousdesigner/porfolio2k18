ff721437ca1c82130fc60c93b154a7cc72dc5259
ferrousdesigner <ferrousdesigner@gmail.com>
2018-02-14 21:23:29 +0530

    Project pics addition, links working

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ let current_top = 0
 let new_top = 0
 let new_left = 0
 
+let timeout = window.innerWidth > 800 ? 1000 : 600
 
 MOVING_X = true
 
@@ -104,7 +105,7 @@ function slideDown () {
   panelV.style.transform = 'translateY(-' + new_top + 'px)'
   current_top = new_top
 
-  setTimeout(function () { MOVING_V = true}, 1000)
+  setTimeout(function () { MOVING_V = true}, timeout)
   }
 }
 
@@ -116,34 +117,34 @@ function slideUp () {
   panelV.style.transform = 'translateY(-' + new_top + 'px)'
   current_top = new_top
 
-  setTimeout(function () { MOVING_V = true}, 1000)
+  setTimeout(function () { MOVING_V = true}, timeout)
   }
 }
 
 function slideLeft () {
   if(MOVING_X && current_left > 0 && current_top >= (3 * window.screen.availHeight) && current_top < (4 * window.screen.availHeight)  ) {
   MOVING_X = false
-  new_left = (parseInt(current_left) - window.screen.availWidth)
+  new_left = (parseInt(current_left) - window.innerWidth)
   panel.style.transform = 'translateX(-' + new_left + 'px)'
   current_left = new_left
   document.getElementsByClassName('active')[0].previousSibling.classList.add('active')
   document.getElementsByClassName('active')[1].classList.remove('active')
   checkArrow()
-  setTimeout(function () { MOVING_X = true}, 1000)
+  setTimeout(function () { MOVING_X = true}, timeout)
   }
 }
 
 function slideRight() {
-  if(MOVING_X && current_left < (7 * window.screen.availWidth) && current_top >= (3 * window.screen.availHeight) && current_top < (4 * window.screen.availHeight) ) {
+  if(MOVING_X && current_left < (7 * window.innerWidth) && current_top >= (3 * window.screen.availHeight) && current_top < (4 * window.screen.availHeight) ) {
   MOVING_X = false
-  new_left = (parseInt(current_left) + window.screen.availWidth)
+  new_left = (parseInt(current_left) + window.innerWidth)
   panel.style.transform = 'translateX(-' + new_left + 'px)'
   current_left = new_left
   document.querySelectorAll('.active')[0].nextSibling.classList.add('active')
   document.querySelectorAll('.active')[0].classList.remove('active')
   checkArrow()
 
-  setTimeout(function () { MOVING_X = true}, 1000)
+  setTimeout(function () { MOVING_X = true}, timeout)
   }
 }
 
@@ -156,7 +157,7 @@ function slideRight() {
       distY,
       threshold = 50, //required min distance traveled to be considered swipe
       restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-      allowedTime = 500, // maximum time allowed to travel that distance
+      allowedTime = 1000, // maximum time allowed to travel that distance
       elapsedTime,
       startTime,
       handleswipe = callback || function(swipedir){}
@@ -200,7 +201,6 @@ document.querySelector('.arrow-right > img').onclick = () => {
     slideLeft()
 }
 
-console.log(document.getElementsByClassName('page-indicator')[0].childNodes)
 
 let indicators = document.getElementsByClassName('page-indicator')[0].childNodes
 
@@ -212,28 +212,75 @@ for(let i = 0; i < indicators.length; i++)
 
 function indicatorAction(e) {
   let offset = e.target.getAttribute('data-index')
-  console.log('pehle', current_left)
   document.getElementsByClassName('active')[0].classList.remove('active')
   e.target.classList.add('active')
-  current_left = (parseInt(offset) - 1) * parseInt(window.screen.availWidth);
-  console.log('ab',current_left)
+  current_left = (parseInt(offset) - 1) * parseInt(window.innerWidth);
   panel.style.transform = 'translateX(-' + current_left + 'px)'
   checkArrow()
 
+}
+
+function moveTo(dest) {
+
+  if(dest == 'home') {
+    current_top = 0 * parseInt(window.screen.availHeight)
+    panelV.style.transform = 'translateY(-' + current_top + 'px)'
+    checkArrow()
+
+  } else if(dest == 'about') {
+    current_top = 1 * parseInt(window.screen.availHeight)
+    panelV.style.transform = 'translateY(-' + current_top + 'px)'
+    checkArrow()
+  } else if(dest == 'theory') {
+    current_top = 2 * parseInt(window.screen.availHeight)
+
+    panelV.style.transform = 'translateY(-' + current_top + 'px)'
+    checkArrow()
+  } else if(dest == 'work') {
+    current_top = 3 * parseInt(window.screen.availHeight)
+
+    panelV.style.transform = 'translateY(-' + current_top + 'px)'
+    checkArrow()
+  } else if(dest == 'hobbie') {
+    current_top = 4 * parseInt(window.screen.availHeight)
+
+    panelV.style.transform = 'translateY(-' + current_top + 'px)'
+    checkArrow()
+  } else if(dest == 'contact') {
+    current_top = 5 * parseInt(window.screen.availHeight)
+
+    panelV.style.transform = 'translateY(-' + current_top + 'px)'
+    checkArrow()
+  }
+  document.querySelector('.close-nav').click()
+
+}
+function goTo(dest) {
+
+  if(dest == 'github'){
+    window.open('https://github.com/ferrousdesigner', '_blank');
+  } else if(dest == 'linkedin'){
+    window.open('https://www.linkedin.com/in/farazkhanuiux', '_blank');
+
+  } else if(dest == 'youtube'){
+    window.open('http://www.youtube.com/channel/UC2BBAZuEc-j25f8CVULJ5eA?sub_confirmation=1', '_blank');
+  } else if(dest == 'instagram'){
+      window.open('https://www.instagram.com/the_band_of_one/', '_blank');
+  }
 }
 
 function checkArrow () {
   if(current_left > 0){
     document.getElementsByClassName('arrow-right')[0].classList.remove('hide-arrow')
   }
-  if(current_left == (7 * window.screen.availWidth))
+  if(current_left == (7 * window.innerWidth))
   {
     document.getElementsByClassName('arrow-left')[0].classList.add('hide-arrow')
   }
   if(current_left == 0){
     document.getElementsByClassName('arrow-right')[0].classList.add('hide-arrow')
   }
-  if(current_left < (7 * window.screen.availWidth))
+  if(current_left < (7 * window.innerWidth))
   {
     document.getElementsByClassName('arrow-left')[0].classList.remove('hide-arrow')
   }
